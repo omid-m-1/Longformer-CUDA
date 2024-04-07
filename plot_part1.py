c2d7dcac61752b2540a25b9b9f9ecbe3b8a20fdb
omid-m-1 <mersa.omid@gmail.com>
2024-04-07 13:07:02 -0400

    kernel running time

---
+++ plot_part1.py
@@ -0,0 +1,144 @@
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.rcParams.update({'font.size': 14})
+
+with open('./test/list_seq_dcg.pkl', 'rb') as f:
+    list_seq_dcg = pickle.load(f)
+with open('./test/list_seq_dense.pkl', 'rb') as f:
+    list_seq_dense = pickle.load(f)
+with open('./test/list_seq_tvm.pkl', 'rb') as f:
+    list_seq_tvm = pickle.load(f)
+list_seq = []
+list_seq += list_seq_dcg + list_seq_dense + list_seq_tvm
+
+with open('./test/list_batch_dcg.pkl', 'rb') as f:
+    list_batch_dcg = pickle.load(f)
+with open('./test/list_batch_dense.pkl', 'rb') as f:
+    list_batch_dense = pickle.load(f)
+with open('./test/list_batch_tvm.pkl', 'rb') as f:
+    list_batch_tvm = pickle.load(f)
+list_batch = []
+list_batch += list_batch_dcg + list_batch_dense + list_batch_tvm
+
+with open('./test/list_window_dcg.pkl', 'rb') as f:
+    list_window_dcg = pickle.load(f)
+with open('./test/list_window_dense.pkl', 'rb') as f:
+    list_window_dense = pickle.load(f)
+with open('./test/list_window_tvm.pkl', 'rb') as f:
+    list_window_tvm = pickle.load(f)
+list_window = []
+list_window += list_window_dcg + list_window_dense + list_window_tvm
+
+with open('./test/list_dilation_dcg.pkl', 'rb') as f:
+    list_dilation_dcg = pickle.load(f)
+with open('./test/list_dilation_dense.pkl', 'rb') as f:
+    list_dilation_dense = pickle.load(f)
+with open('./test/list_dilation_tvm.pkl', 'rb') as f:
+    list_dilation_tvm = pickle.load(f)
+list_dilation = []
+list_dilation += list_dilation_dcg + list_dilation_dense + list_dilation_tvm
+
+def average(data, filter_condition):
+    filtered_data = [record for record in data if filter_condition(record)]
+    if not filtered_data:
+        return None
+    cuda_times = [float(record[7].replace('us', '')) for record in filtered_data if record[7] is not None]
+    if len(cuda_times) != len(filtered_data):
+        return None
+    return np.mean(cuda_times)
+
+kernels = ['tvm', 'dense', 'dcg']
+modes = [1, 3]
+heads = [12, 24]
+dilations = range(1,5)
+colors = {'tvm': 'blue', 'dense': 'green', 'dcg': 'red'}
+styles = {'tvm': '-', 'dense': '--', 'dcg': '-.'}
+
+fig, axs = plt.subplots(2, 2, figsize=(14, 12))
+# Plot 1: Sequence Length
+sequence_lengths = range(512, 16385, 512)
+for kernel in kernels:
+    times = [average(list_seq, lambda record:
+        record[0] == kernel and
+        record[1] in modes and
+        record[2] == 2 and
+        record[3] in heads and
+        record[4] == 256 and
+        record[5] in dilations and
+        record[6] == seq_len)
+        for seq_len in sequence_lengths]
+    if any(time is not None for time in times):
+        axs[0, 0].plot(sequence_lengths, times, label=kernel, color=colors[kernel], linestyle=styles[kernel], linewidth = 3)
+axs[0, 0].set_xlabel('Sequence Length')
+axs[0, 0].set_ylabel('CUDA Time (microseconds)')
+axs[0, 0].set_title('CUDA Running Time vs Sequence Length')
+axs[0, 0].legend()
+# Plot 2: Batch Size
+batch_sizes = range(1, 17)
+for kernel in kernels:
+    times = [average(list_batch, lambda record:
+        record[0] == kernel and
+        record[1] in modes and
+        record[2] == batch_size and
+        record[3] in heads and
+        record[4] == 256 and
+        record[5] in range(1, 5) and
+        record[6] == 4096)
+        for batch_size in batch_sizes]
+    if any(time is not None for time in times):
+        axs[0, 1].plot(batch_sizes, times, label=kernel, color=colors[kernel], linestyle=styles[kernel], linewidth = 3)
+axs[0, 1].set_xlabel('Batch Size')
+axs[0, 1].set_ylabel('CUDA Time (microseconds)')
+axs[0, 1].set_title('CUDA Running Time vs Batch Size')
+axs[0, 1].legend()
+# Plot 3: Window Length
+window_lengths = range(64, 1025, 64)
+for kernel in kernels:
+    times = [average(list_window, lambda record:
+        record[0] == kernel and
+        record[1] in modes and
+        record[2] == 2 and
+        record[3] in heads and
+        record[4] == window_len and
+        record[5] in dilations and
+        record[6] == 4096)
+        for window_len in window_lengths]
+    if any(time is not None for time in times):
+        axs[1, 0].plot(window_lengths, times, label=kernel, color=colors[kernel], linestyle=styles[kernel], linewidth = 3)
+axs[1, 0].set_xlabel('Window Length')
+axs[1, 0].set_ylabel('CUDA Time (microseconds)')
+axs[1, 0].set_title('CUDA Running Time vs Window Length')
+axs[1, 0].legend()
+# Plot 3: Dilation
+for kernel in kernels:
+    times = [average(list_dilation, lambda record:
+        record[0] == kernel and
+        record[1] in modes and
+        record[2] == 2 and
+        record[3] in heads and
+        record[4] == 256 and
+        record[5] == dilation and
+        record[6] == 4096)
+        for dilation in dilations]
+    if any(time is not None for time in times):
+        axs[1, 1].plot(dilations, times, label=kernel, color=colors[kernel], linestyle=styles[kernel], linewidth = 3)
+axs[1, 1].set_xticks([1, 2, 3, 4])
+axs[1, 1].set_xlabel('Dilation')
+axs[1, 1].set_ylabel('CUDA Time (microseconds)')
+axs[1, 1].set_title('CUDA Running Time vs Dilation Rate')
+axs[1, 1].legend()
+
+subplot_labels = ['a', 'b', 'c', 'd']
+for i, ax in enumerate(axs.flat):
+    ax.text(-0.15, 1.1, subplot_labels[i], transform=ax.transAxes, fontsize=16, fontweight='bold', va='top')
+
+plt.tight_layout()
+plt.subplots_adjust(hspace=0.4)
+
+for ax in axs.flat:
+    for axis in ['top', 'bottom', 'left', 'right']:
+        ax.spines[axis].set_linewidth(2)
+
+plt.savefig('Figure2.pdf', bbox_inches='tight')
---
+++ test_unit_part1.py
@@ -0,0 +1,93 @@
+import torch
+from longformer_util.diagonaled_mm_tvm import diagonaled_mm
+from torch.profiler import profile, ProfilerActivity
+import importlib
+import pickle
+import argparse
+
+lformerMM = importlib.import_module("longformer_util.deep-codegen.pytorch_apis").lformerMM
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--kernel', default='dcg', help='valid options: [tvm, dcg, dense]')
+parser.add_argument('--type', default='seq', help='valid options: [seq, batch, window, dilation]')
+args = parser.parse_args()
+
+# parameter ranges
+sequence_lengths = range(512, 16385, 512) if args.type == 'seq' else [4096]
+batch_sizes = range(1, 17) if args.type == 'batch' else [2]
+head_numbers = range(12, 25, 12)
+window_lengths = range(64, 1025, 64) if args.type == 'window' else [256]
+dilations = range(1, 5)
+
+if args.kernel not in ['tvm', 'dcg', 'dense']:
+    raise ValueError(f'{args.kernel} kernel is not defined')
+
+if args.type not in ['seq', 'batch', 'window', 'dilation']:
+    raise ValueError(f'{args.type} type is not defined')
+
+running_times = []
+for batch_size in batch_sizes:
+    for head_number in head_numbers:
+        for window_length in window_lengths:
+            for dilation in dilations:
+                for sequence_length in sequence_lengths:
+                    for kernel in args.kernels:
+                        for mode in [1, 3]:
+                            input1_dimensions = [batch_size, sequence_length, head_number, -1]
+                            input2_dimensions = [batch_size, sequence_length, head_number, 64]
+                            padding = 0
+                            autoregressive = False
+                            window_upper = 0 if autoregressive else window_length
+                            is_diagonal = False if mode == 3 else True
+                            if mode == 1:
+                                if kernel != 'dense':
+                                    input1_dimensions[3] = window_length + window_upper + 1
+                                else:
+                                    input1_dimensions[3] = sequence_length
+                            else:
+                                input1_dimensions[3] = input2_dimensions[3]
+                            try:
+                                input1 = torch.rand(input1_dimensions, requires_grad=True, device=device).contiguous()
+                                input2 = torch.rand(input2_dimensions, requires_grad=True, device=device).contiguous()
+                                dilation_tensor = torch.tensor([dilation]*head_number, dtype=torch.int, requires_grad=False, device=device)
+                                if kernel == 'dcg':
+                                    output = lformerMM(input1, input2, window_length, dilation_tensor, is_diagonal, autoregressive)
+                                    output = lformerMM(input1, input2, window_length, dilation_tensor, is_diagonal, autoregressive)
+                                elif kernel == 'tvm':
+                                    output = diagonaled_mm(input1, input2, window_length, dilation_tensor, is_diagonal, padding, autoregressive)
+                                    output = diagonaled_mm(input1, input2, window_length, dilation_tensor, is_diagonal, padding, autoregressive)
+                                else:
+                                    if (mode == 1):
+                                        output = torch.einsum('bxhy,byhd->bxhd', (input1, input2))
+                                        output = torch.einsum('bxhy,byhd->bxhd', (input1, input2))
+                                    else:
+                                        output = torch.einsum('bxhd,byhd->bxhy', (input1, input2))
+                                        output = torch.einsum('bxhd,byhd->bxhy', (input1, input2))
+                                random_target = torch.rand_like(output, device=device)
+                                with profile(activities=[ProfilerActivity.CUDA], record_shapes=True) as prof:
+                                    if kernel == 'dcg':
+                                        output = lformerMM(input1, input2, window_length, dilation_tensor, is_diagonal, autoregressive)
+                                    elif kernel == 'tvm':
+                                        output = diagonaled_mm(input1, input2, window_length, dilation_tensor, is_diagonal, padding, autoregressive)
+                                    else:
+                                        if (mode == 1) :
+                                            output = torch.einsum('bxhy,byhd->bxhd', (input1, input2))
+                                        else:
+                                            output = torch.einsum('bxhd,byhd->bxhy', (input1, input2))
+                                    loss = (output - random_target).pow(2).mean()
+                                    loss.backward()
+                                cuda_time = sum([event.cuda_time_total for event in prof.key_averages()])
+                                cpu_time = sum([event.cpu_time_total for event in prof.key_averages()])
+                                running_times.append([kernel, mode, batch_size, head_number, window_length, dilation, sequence_length, f'{cuda_time}us', f'{cpu_time}us'])
+                            except RuntimeError as e:
+                                if 'out of memory' in str(e):
+                                    running_times.append([kernel, mode, batch_size, head_number, window_length, dilation, sequence_length, None, None])
+                                else:
+                                    raise e
+                            torch.cuda.synchronize()
+                            torch.cuda.empty_cache()
+
+os.makedirs('./test', exist_ok=True)
+with open(f'./test/list_{args.type}_{args.kernel}.pkl', 'wb') as f:
+    pickle.dump(running_times, f)
